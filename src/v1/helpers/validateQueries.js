ed9247a9c1da2753933143471968dcc424d7a212
Guilherme Joviniano <00drpixelss@gmail.com>
2022-09-25 12:01:43 -0700

    adjusting the validate querie functuin

--- src/v1/helpers/validateQueries.js
+++ src/v1/helpers/validateQueries.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line max-len
-const validateQueries = (queries) => Object.keys(queries).forEach((key) => (key !== 'course' || key !== 'status' || key !== 'year'));
+const validateQueries = (queries) => Object.keys(queries).map((key) => key !== 'course' || key !== 'status' || key !== 'year');
 
 export default validateQueries;
